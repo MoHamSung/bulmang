37a3f4e5ffd9f7417d0a60fd5c3a4ab06a826bac
bulmang <hmk113737@gmail.com>
2023-05-22 01:23:53 +0900

    [Feat] 애플스토어 구매 UI

--- act2/AppleStore/AppleStore/View/AppleStoreBottomView.swift
+++ act2/AppleStore/AppleStore/View/AppleStoreBottomView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct AppleStoreBottomView: View {
+    
+    let price: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center,spacing: 0){
+            
+            Text(price)
+                .font(.system(size: 10))
+                .fontWeight(.light)
+            
+            Spacer()
+            
+            Text("구입하기")
+                .font(.system(size: 13))
+                .fontWeight(.bold)
+                .foregroundColor(Color("AppleStoreBlueColor"))
+                .padding(.vertical,6)
+                .padding(.horizontal,16)
+                .background(Color("ColorButtonGray"))
+                .cornerRadius(17)
+        }
+        .padding(.horizontal,29)
+        .frame(maxWidth: .infinity)
+        .frame(height: 67)
+        .background(Color("BottomColor"))
     }
 }
 
 struct AppleStoreBottomView_Previews: PreviewProvider {
     static var previews: some View {
-        AppleStoreBottomView()
+        AppleStoreMainView()
     }
 }
