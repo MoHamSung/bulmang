82fed58e2fad5428e0fee035d59fe83650ac3ecd
bulmang <hmk113737@gmail.com>
2023-05-22 01:25:25 +0900

    [Feat] 애플스토어 Slide UI

--- act2/AppleStore/AppleStore/View/AppleStoreSliderView.swift
+++ act2/AppleStore/AppleStore/View/AppleStoreSliderView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct AppleStoreSliderView: View {
+
+    var indicator = UIPageControl.appearance()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        
+            TabView {
+                ForEach(slides) { slide in
+                    VStack {
+                        HStack(alignment: .top,spacing: 0){
+                            Image(systemName: slide.imageName)
+                                .font(.system(size: slide.fontSize))
+                                .padding(.trailing,12)
+                                .padding(.leading,20)
+                            
+                            VStack(alignment: .leading,spacing:0){
+                                Text(slide.title)
+                                    .font(.system(size: 13))
+                                    .fontWeight(.bold)
+                                    .padding(.bottom,4)
+                                    
+                                Text(slide.subTitle)
+                                    .font(.system(size: 12))
+                                    .fontWeight(.regular)
+                                    .padding(.bottom,4)
+                                    .frame(maxWidth: .infinity,alignment:.leading)
+                                    
+                                Text("더 알아보기")
+                                    .foregroundColor(Color("AppleStoreBlueColor"))
+                                    .font(.system(size: 12))
+                                    .fontWeight(.regular)
+                                    
+                            }
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 20)
+                    
+                }
+            }
+            .frame(height: 138)
+            .tabViewStyle(.page)
+            .onAppear {
+                indicator.currentPageIndicatorTintColor = .black
+                indicator.pageIndicatorTintColor = .gray
+        }
+            
+
+        
     }
 }
 
