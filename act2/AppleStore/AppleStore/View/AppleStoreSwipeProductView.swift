5d2d56a60d597ec8e65239ae75016424810fe13d
bulmang <hmk113737@gmail.com>
2023-05-22 01:26:05 +0900

    [Feat] SwipeProduct UI

--- act2/AppleStore/AppleStore/View/AppleStoreSwipeProductView.swift
+++ act2/AppleStore/AppleStore/View/AppleStoreSwipeProductView.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct AppleStoreSwipeProductView: View {
+    
+    var swipeIndicator = UIPageControl.appearance()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            Text("추천")
+                .font(.system(size: 24))
+                .fontWeight(.heavy)
+                .frame(maxWidth: .infinity,alignment: .leading)
+                .padding(.top,38)
+                .padding(.bottom,38)
+                .padding(.leading)
+            
+            TabView {
+                ForEach(swipes){ swipe in
+                    VStack(spacing: 0) {
+
+                        Image(swipe.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .overlay{
+                                VStack(alignment:.leading,spacing: 0){
+                                    
+                                    Text(swipe.title)
+                                        .font(.system(size: 26))
+                                        .foregroundColor(swipe.textColor)
+                                        .fontWeight(.heavy)
+                                        .padding(.top,38)
+                                        
+                                    
+                                    Text(swipe.subTitle)
+                                        .padding(.top,8)
+                                        .foregroundColor(swipe.textColor)
+                                    Spacer()
+                                }
+                                .padding(.leading,20)
+                                .frame(maxWidth: .infinity,alignment: .leading)
+
+                            }
+                    }
+                    .padding(.horizontal,20)
+                }
+            }
+            
+            .tabViewStyle(.page(indexDisplayMode: .never)) 
+            .frame(height: 509)
+            .shadow(radius: 5,x:1,y:1)
+            .tabViewStyle(.page)
+
+            
+            
+            Spacer()
+        }
     }
 }
 
