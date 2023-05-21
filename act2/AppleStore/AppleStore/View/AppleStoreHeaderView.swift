9bc5ce3ed2d1a1ff480d0c66c2a904b17ec99b01
bulmang <hmk113737@gmail.com>
2023-05-22 01:24:27 +0900

    [Feat] 애플스토어 Header UI

--- act2/AppleStore/AppleStore/View/AppleStoreHeaderView.swift
+++ act2/AppleStore/AppleStore/View/AppleStoreHeaderView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct AppleStoreHeaderView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    var body: some View{
+        HStack(spacing: 0){
+            Text("쇼핑하기")
+                .font(.system(size: 30))
+                .fontWeight(.semibold)
+                .padding(.top,54)
+            Spacer()
+            Image("AppleStoreLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 37, height: 36)
+                .padding(.top, 48)
+        }
+        .frame(height: 100)
+        .padding(.bottom,10)
+        
     }
 }
 
