a5edb675c3950d0a72d166605c825a5e6cb44c4b
bulmang <hmk113737@gmail.com>
2023-05-24 23:53:07 +0900

    [Chore] MainView

--- act2/AppleStore/AppleStore/View/AppleStoreMainView.swift
+++ act2/AppleStore/AppleStore/View/AppleStoreMainView.swift
@@ -11,34 +11,49 @@ struct AppleStoreMainView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                    VStack (alignment:.leading,spacing: 0) {
-
-                        TopView
-                            .padding(.horizontal,20)
-                        
-                        AppleStoreSliderView()
-                                        
-                        AppleStoreImagePageView(itemName: "iPhone 14", itemDescription1: "iPhone 14 그리고 큼직한 사이즈의 iPhone 14 Plus. 획기적인", itemDescription2: "도약을 이뤄낸 배터리 성능. 프로급 카메라. 6가지 멋진 컬러.", imageName: "AppleStoreImage1")
-                        
-                        AppleStoreBottomView(price: "₩1,250,000부터")
-                        
-                        AppleStoreImagePageView(itemName: "iPad", itemDescription1: "다양한 일상 작업에 맞는 완전히 새롭고, 컬러풀한 iPad. 새로운", itemDescription2: "iPad를 소개합니다.", imageName: "AppleStoreImage2")
-                        
-                        AppleStoreBottomView(price: "₩1,250,000부터")
-                        
-                        AppleStoreSwipeProductView()
-                    }
+                VStack (alignment:.leading,spacing: 0) {
+                    
+                    TopView
+                        .padding(.horizontal,20)
+                    
+                    AppleStoreSliderView()
+                    
+                    AppleStoreImagePageView(itemName: "iPhone 14", itemDescription1: "iPhone 14 그리고 큼직한 사이즈의 iPhone 14 Plus. 획기적인", itemDescription2: "도약을 이뤄낸 배터리 성능. 프로급 카메라. 6가지 멋진 컬러.", imageName: "AppleStoreImage1")
+                    
+                    AppleStoreBottomView(price: "₩1,250,000부터")
+                    
+                    AppleStoreImagePageView(itemName: "iPad", itemDescription1: "다양한 일상 작업에 맞는 완전히 새롭고, 컬러풀한 iPad. 새로운", itemDescription2: "iPad를 소개합니다.", imageName: "AppleStoreImage2")
+                    
+                    AppleStoreBottomView(price: "₩1,250,000부터")
+                    
+                    AppleStoreSwipeProductView()
+                }
+                
             }
-            
-
         }
+        
+        
+        
         
         
         
     }
     @ViewBuilder private var TopView: some View {
         
-        AppleStoreHeaderView()
+        HStack(spacing: 0){
+            Text("쇼핑하기")
+                .font(.system(size: 30))
+                .fontWeight(.semibold)
+                .padding(.top,54)
+            Spacer()
+            Image("AppleStoreLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 37, height: 36)
+                .padding(.top, 48)
+        }
+        .frame(height: 100)
+        .padding(.bottom,10)
         
         Rectangle()
             .foregroundColor(Color("Divider"))
