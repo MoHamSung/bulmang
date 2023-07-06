549d4073e89fa24e1fae954a424708f2d4151eaa
bulmang <hmk113737@gmail.com>
2023-07-07 08:23:44 +0900

    [Add] Image & Color Asset

---
+++ act5/NaverWebtoon/NaverWebtoon/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NaverWebtoon
+//
+//  Created by 하명관 on 2023/07/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ act5/NaverWebtoon/NaverWebtoon/NaverWebtoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverWebtoonApp.swift
+//  NaverWebtoon
+//
+//  Created by 하명관 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct NaverWebtoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
