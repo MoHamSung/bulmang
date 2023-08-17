a692bb124d1b32b82e8bded61337e18fbfe59a14
bulmang <hmk113737@gmail.com>
2023-08-17 11:24:45 +0900

    [Add] - AppViewModel 생성 - 화면 전환에 사용되는 데이터를 관리해주는 모델

---
+++ act6/KakaoBank/KakaoBank/ViewModel/AppViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AppViewModel.swift
+//  KakaoBank
+//
+//  Created by 하명관 on 2023/08/16.
+//
+
+import Foundation
+
+class AppViewModel: ObservableObject {
+    @Published var isDetailActive: Bool = false
+}
