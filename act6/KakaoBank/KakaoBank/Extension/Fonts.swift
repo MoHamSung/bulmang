f01a9fda56a59797244e39494692ff724255ea90
bulmang <hmk113737@gmail.com>
2023-08-20 02:21:30 +0900

    [Add] - Font Extension 생성

--- act6/KakaoBank/KakaoBank/Extension/Fonts.swift
+++ act6/KakaoBank/KakaoBank/Extension/Fonts.swift
@@ -9,10 +9,22 @@ import Foundation
 import SwiftUI
 
 extension Font {
-    static func customFont(size: CGFloat, weight: Font.Weight) -> Font {
-        return Font.system(size: size).weight(weight)
-    }
+    static let kakaoHeadlineSemiBold = Font.custom("Pretendard-SemiBold", size: 23)
+    static let kakaoCapsuleSemiBold = Font.custom("Pretendard-SemiBold", size: 13)
+
+    static let kakaoCaptionMedium = Font.custom("Pretendard-Medium", size: 14)
+    static let kakaoCaptionSemiBold = Font.custom("Pretendard-SemiBold", size: 14)
+    static let kakaoCaptionRegular = Font.custom("Pretendard-Regular", size: 14)
+
+    static let kakaoMoneyBold = Font.custom("Pretendard-Bold", size: 19)
+    static let kakaoMoneyBigBold = Font.custom("Pretendard-Bold", size: 35)
+    static let kakaoMoneySemiBold = Font.custom("Pretendard-SemiBold", size: 16)
+    static let kakaoMoneyDealBold = Font.custom("Pretendard-Bold", size: 17)
+    static let kakaoMoneyRemainRegular = Font.custom("Pretendard-Regular", size: 14)
+
+    static let kakaoNameSemiBold = Font.custom("Pretendard-Regular", size: 14)
 }
+
 // font-weight: 100; (Thin)
 // font-weight: 200; (Extra Light)
 // font-weight: 300; (Light)
@@ -23,15 +35,4 @@ extension Font {
 // font-weight: 800; (Extra Bold)
 // font-weight: 900; (Black)
 
-let kakaoHeadlineSemiBold = Font.customFont(size: 23, weight: .semibold)
-let kakaoCapsuleSemiBold = Font.customFont(size: 13, weight: .semibold)
 
-let kakaoCaptionMedium = Font.customFont(size: 14, weight: .medium)
-let kakaoCaptionSemiBold = Font.customFont(size: 14, weight: .semibold)
-let kakaoCaptionRegular = Font.customFont(size: 14, weight: .regular)
-
-let kakaoMoneyBold = Font.customFont(size: 19, weight: .bold)
-let kakaoMoneyBigBold = Font.customFont(size: 35, weight: .bold)
-let kakaoMoneySemiBold = Font.customFont(size: 16, weight: .semibold)
-
-let kakaoNameSemiBold = Font.customFont(size: 14, weight: .regular)
